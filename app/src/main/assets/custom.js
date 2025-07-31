2c2e3ebd5736a4da0154636e066a9fd2a25147fb
zxc4we-lab <zxc4we028@gmail.com>
2025-07-31 22:08:23 +0700

    update custom js from pakeplus

--- app/src/main/assets/custom.js
+++ app/src/main/assets/custom.js
@@ -1,26 +1,53 @@
-//console.log(
-//    '%cbuild from PakePlus： https://github.com/Sjj1024/PakePlus',
-//    'color:orangered;font-weight:bolder'
-//)
-//
-//// very important, if you don't know what it is, don't touch it
-//// 非常重要，不懂代码不要动
-//const hookClick = (e) => {
-//    const origin = e.target.closest('a')
-//    const isBaseTargetBlank = document.querySelector(
-//        'head base[target="_blank"]'
-//    )
-//    console.log('origin', origin, isBaseTargetBlank)
-//    if (
-//        (origin && origin.href && origin.target === '_blank') ||
-//        (origin && origin.href && isBaseTargetBlank)
-//    ) {
-//        e.preventDefault()
-//        console.log('handle origin', origin)
-//        location.href = origin.href
-//    } else {
-//        console.log('not handle origin', origin)
-//    }
-//}
-//
-//document.addEventListener('click', hookClick, { capture: true })
+console.log(
+    '%cbuild from PakePlus： https://github.com/Sjj1024/PakePlus',
+    'color:orangered;font-weight:bolder'
+)
+
+// very important, if you don't know what it is, don't touch it
+// 非常重要，不懂代码不要动，这里可以解决80%的问题，也可以生产1000+的bug
+const hookClick = (e) => {
+    const origin = e.target.closest('a')
+    const isBaseTargetBlank = document.querySelector(
+        'head base[target="_blank"]'
+    )
+    console.log('origin', origin, isBaseTargetBlank)
+    if (
+        (origin && origin.href && origin.target === '_blank') ||
+        (origin && origin.href && isBaseTargetBlank)
+    ) {
+        e.preventDefault()
+        console.log('handle origin', origin)
+        location.href = origin.href
+    } else {
+        console.log('not handle origin', origin)
+    }
+}
+
+window.open = function (url, target, features) {
+    console.log('open', url, target, features)
+    location.href = url
+}
+
+document.addEventListener('click', hookClick, { capture: true })
+
+// css filter
+document.addEventListener('DOMContentLoaded', () => {
+    const targetNode = document.body
+    // 配置观察选项
+    const config = {
+        childList: true,
+        subtree: true,
+    }
+    const observer = new MutationObserver((mutationsList, observer) => {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                const element0 = document.querySelector('Create AI Music');
+                if (element0) {
+                    element0.style.display = 'none';
+                }
+            }
+        }
+    })
+    observer.observe(targetNode, config)
+})
+// end css filter
